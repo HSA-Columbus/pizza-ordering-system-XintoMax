166d42d73033b23ed402320b68bc99d3c746a2cd
Tyler.Fowsantia <student.t.fowsantia@horizoncolumbus.org>
2020-01-30 09:11:42 -0500

    base code

---
+++ main.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from flask import *
+
+
+class CustomerInfo:
+    name = ""
+    address = ""
+    pizza_type = []
+    pizza_topping = []
+    price = 0
+    number_of_pizza = 0
+    total = 0
+    calorie = 0
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html", info=CustomerInfo)
+
+
+@app.route('/order_placed')
+def order():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
