971e5571262ecb273e843f3b37e5c3b4aca66aae
Tyler.Fowsantia <student.t.fowsantia@horizoncolumbus.org>
2020-02-14 09:10:18 -0500

    code update and database

--- application.py
+++ application.py
@@ -1,29 +1,53 @@
 from abc import ABC, abstractmethod
 from flask import *
-
-
-class CustomerInfo:
-    name = ""
-    address = ""
-    pizza_type = []
-    pizza_topping = []
-    price = 0
-    number_of_pizza = 0
-    total = 0
-    calorie = 0
-
+import sqlite3
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html", info=CustomerInfo)
+    if request.method == "POST":
+        with sqlite3.connect("checkout") as conn:
+            command = "INSERT INTO stuff VALUES(?, ?, ?)"
+            data_list = []
+            data_list.append('')
+            data_list.append(1)
+            data_list.append('')
+            conn.execute(command, data_list)
+            conn.commit()
+        return render_template("home.html")
+    else:
+        return render_template("home.html")
 
 
-@app.route('/order_placed')
+@app.route('/checkout', methods=['POST'])
 def order():
-    render_template("orderfinish.html")
+    with sqlite3.connect("checkout") as conn:
+        pizza_in = request.args.get('group1')
+        if pizza_in == 'input1':
+            pizza = "Sicilian"
+            price = "$7"
+        elif pizza_in == 'input2':
+            pizza = "Marine"
+            price = "$7"
+        elif pizza_in == 'input3':
+            pizza = "Neapolitan"
+            price = "$7"
+        elif pizza_in == 'input4':
+            pizza = "Mixed"
+            price = "$8"
+        command = "INSERT INTO stuff VALUES(?, ?, ?)"
+        data_list = []
+        data_list.append(pizza)
+        data_list.append(1)
+        data_list.append(price)
+        conn.execute(command, data_list)
+        conn.commit()
+        command1 = "SELECT * FROM stuff"
+        cursor1 = conn.execute(command1)
+        table_assignments = cursor1.fetchall()
+    render_template("checkout.html", table_assignments=table_assignments)
 
 
 if __name__ == "__main__":
